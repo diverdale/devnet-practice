c3076a719e0391f57ce7385db60fb818a05e0b2f
dalwrigh <dalewrigh@cisco.com>
2022-03-09 09:18:02 -0500

    tweaking json

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import requests
+import pprint
 import json
 import urllib3
 from config import *
 
+pp = pprint.PrettyPrinter(indent=4)
+
 urllib3.disable_warnings()
 
 url = BASE_URL + '?lat=' + LATITUDE + '&lon=' + LONGITUDE + '&appid=' + API_KEY
-print(url)
 weather = requests.get(url, verify=False)
-print(weather.json())
+pp.pprint(weather.json())
