f191ab117aeae623e15458f164292d1920346e6f
dalwrigh <dalewrigh@cisco.com>
2022-03-13 14:20:57 -0400

    updating var names

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ pp = pprint.PrettyPrinter(indent=4)
 urllib3.disable_warnings()
 
 url = BASE_URL + '?lat=' + LATITUDE + '&lon=' + LONGITUDE + '&exclude=minutely,hourly&units=imperial&appid=' + API_KEY
-weather = requests.get(url, verify=False)
-print(weather.json()['current'])
+weather_dale = requests.get(url, verify=False)
+print(weather_dale.json()['current'])
 #pp.pprint(weather.json())
